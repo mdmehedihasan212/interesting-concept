6f2e0f6ebf43aba542167a0790ff9473eda719d8
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-02-26 02:15:32 +0600

    add callback

---
+++ callback.js
@@ -0,0 +1,21 @@
+// function callBack(name) {
+//     console.log(name);
+// }
+// const actor = 'tom cruse';
+// callBack(actor);
+
+// function action(doom) {
+//     console.log(doom);
+// }
+// const abraham = ['doom', 'force', 'force2', 'tereliyan'];
+// action(abraham);
+
+// 
+function myObject(developer) {
+    console.log(developer);
+    developer();
+}
+function myGoal() {
+    console.log('my dream is web developer')
+};
+myObject(myGoal);
